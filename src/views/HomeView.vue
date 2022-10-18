472acd71ff155d73265bdfb7f3089cea5c07dc39
MatheusOsterno <matheusosternodev@gmail.com>
2022-10-17 22:58:16 -0300

    Projeto em andamento

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
+
+// <projectRoot>/src/views/.eslintrc.js
+  module.exports = {
+    rules: {
+      'vue/multi-word-component-names': 0,
+    },
+  }
